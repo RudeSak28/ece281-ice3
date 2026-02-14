ec19e03fe23e629e8fab01142fa8a663e9e856d9
RudeSak28 <C28Saketh.Rudraraju@users.noreply.github.com>
2026-02-13 22:20:41 -0700

    ice3

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -50,11 +50,11 @@ begin
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
 	   w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"0" and w_Cout = '0') report "bad with all zeros and 1 one" severity failure;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad with all zeros and 1 one" severity failure;
 	   w_addends <= x"55"; w_Cin <= '0'; wait for 10 ns;
-	       assert (w_sum = x"0" and w_Cout = '0') report "bad with 01010101" severity failure;
+	       assert (w_sum = x"A" and w_Cout = '0') report "bad with 01010101" severity failure;
 	   w_addends <= x"9F"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"0" and w_Cout = '1') report "bad with 10011111" severity failure;
+	       assert (w_sum = x"9" and w_Cout = '1') report "bad with 10011111" severity failure;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
